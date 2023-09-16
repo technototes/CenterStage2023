9fad684a81def3ab668742ca71aa0ae40a6cfba2
Kevin Frei <kevinfrei@hotmail.com>
2023-09-16 07:41:16 -0700

    Minor reformatting from prettier

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Twenty403Testing.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Twenty403Testing.java
@@ -3,7 +3,6 @@ package com.example.meepmeeptesting;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-
 import java.io.*;
 import javax.imageio.ImageIO;
 
@@ -13,23 +12,23 @@ public class Twenty403Testing {
         // Make this as large as possible while still fitting on our laptop screens:
         MeepMeep meepMeep = new MeepMeep(960);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                .setDimensions(14, 14)
-                // Constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, trackWidth
-                // maxVel: The fastest dist/sec we'll travel (velocity)
-                // maxAcc: The fastest rate (dist/sec/sec) we'll change our velocity (acceleration)
-                // maxAngVel: the fastest degrees/sec we'll rotate (angular velocity)
-                // maxAngAcc: the fastest rate (deg/sec/sec) we'll change our rotation (angular acceleration)
-                // trackWidth: The width of our wheelbase (not clear what this really affects...)
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9.5)
-                .followTrajectorySequence(drive ->
-                        drive
-                                .trajectorySequenceBuilder(AutoConstantsRed.Away.START)
-                                .addTrajectory(AutoConstantsRed.Away.START_TO_LEFT_LOW.get())
-                                //.addTrajectory(AutoConstantsRed.Away.START_TO_RIGHT_LOW.get())
-                                .addTrajectory(AutoConstantsRed.Away.LEFT_LOW_TO_BETWEEN_LEFT.get())
-                                .addTrajectory(AutoConstantsRed.Away.BETWEEN_TO_PARK_LEFT.get())
-                                .build()
-                );
+            .setDimensions(14, 14)
+            // Constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, trackWidth
+            // maxVel: The fastest dist/sec we'll travel (velocity)
+            // maxAcc: The fastest rate (dist/sec/sec) we'll change our velocity (acceleration)
+            // maxAngVel: the fastest degrees/sec we'll rotate (angular velocity)
+            // maxAngAcc: the fastest rate (deg/sec/sec) we'll change our rotation (angular acceleration)
+            // trackWidth: The width of our wheelbase (not clear what this really affects...)
+            .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9.5)
+            .followTrajectorySequence(drive ->
+                drive
+                    .trajectorySequenceBuilder(AutoConstantsRed.Away.START)
+                    .addTrajectory(AutoConstantsRed.Away.START_TO_LEFT_LOW.get())
+                    //.addTrajectory(AutoConstantsRed.Away.START_TO_RIGHT_LOW.get())
+                    .addTrajectory(AutoConstantsRed.Away.LEFT_LOW_TO_BETWEEN_LEFT.get())
+                    .addTrajectory(AutoConstantsRed.Away.BETWEEN_TO_PARK_LEFT.get())
+                    .build()
+            );
         try {
             // Try to load the field image from the repo:
             meepMeep.setBackground(ImageIO.read(new File("Field.jpg")));
