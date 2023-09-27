73d391a9a02edac8e172f9c11e944bfafb1ea93c
Kevin Frei <kevinfrei@hotmail.com>
2023-09-26 21:33:11 -0700

    Students put together the Intake subsystem for 16750

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/Robot.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/Robot.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.sixteen750;
 import com.technototes.library.logger.Loggable;
 import com.technototes.library.util.Alliance;
 import org.firstinspires.ftc.sixteen750.helpers.StartingPosition;
+import org.firstinspires.ftc.sixteen750.subsystems.IntakeSubsystem;
 
 public class Robot implements Loggable {
 
     public StartingPosition position;
     public Alliance alliance;
     public double initialVoltage;
+    public IntakeSubsystem intake;
 
     public Robot(Hardware hw, Alliance team, StartingPosition pos) {
         this.position = pos;
         this.alliance = team;
         this.initialVoltage = hw.voltage();
+        intake = new IntakeSubsystem(hw.leftIntake, hw.rightIntake);
     }
 }
