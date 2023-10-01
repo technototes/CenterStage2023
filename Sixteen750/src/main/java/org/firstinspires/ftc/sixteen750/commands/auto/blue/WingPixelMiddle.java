5f0130d2dcc8928879b4c83b53fcfdd8fe3e8cb5
Kevin Frei <kevinfrei@hotmail.com>
2023-09-30 22:30:00 -0700

    Format cleanup

--- Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/commands/auto/blue/WingPixelMiddle.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/commands/auto/blue/WingPixelMiddle.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.sixteen750.commands.auto.blue;
 
 import com.technototes.library.command.SequentialCommandGroup;
 import com.technototes.path.command.TrajectorySequenceCommand;
-
 import org.firstinspires.ftc.sixteen750.AutoConstants.WingBlue;
 import org.firstinspires.ftc.sixteen750.Robot;
 
@@ -10,13 +9,14 @@ public class WingPixelMiddle extends SequentialCommandGroup {
 
     public WingPixelMiddle(Robot r) {
         super(
-                new TrajectorySequenceCommand(r.drivebase, WingBlue.START_TO_MIDDLE_SPIKE)
+            new TrajectorySequenceCommand(r.drivebase, WingBlue.START_TO_MIDDLE_SPIKE)
+                .andThen(
+                    new TrajectorySequenceCommand(r.drivebase, WingBlue.MIDDLE_SPIKE_TO_CLEAR)
+                        //Add trajectory from clear to park left(also not working)
                         .andThen(
-                                new TrajectorySequenceCommand(r.drivebase, WingBlue.MIDDLE_SPIKE_TO_CLEAR)
-                                        //Add trajectory from clear to park left(also not working)
-                                        .andThen(
-                                                new TrajectorySequenceCommand(r.drivebase, WingBlue.CLEAR_TO_PARK_LEFT)
-                                        )
-                        ));
+                            new TrajectorySequenceCommand(r.drivebase, WingBlue.CLEAR_TO_PARK_LEFT)
+                        )
+                )
+        );
     }
 }
