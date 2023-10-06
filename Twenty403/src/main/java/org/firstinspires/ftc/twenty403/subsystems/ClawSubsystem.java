11eaab113dc3b01c7dce5a2ba811b05ef216010c
Yinn333 <hoda.ata18@gmail.com>
2023-10-06 16:16:54 -0700

    started

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystems/ClawSubsystem.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.twenty403.subsystems;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.technototes.library.logger.Loggable;
+import com.technototes.library.subsystem.Subsystem;
+@Config
+public class ClawSubsystem implements Subsystem, Loggable {
+    public static double OPEN_CLAW_POS = 0;
+    public static double CLOSE_CLAW_POS = 1;
+
+}
