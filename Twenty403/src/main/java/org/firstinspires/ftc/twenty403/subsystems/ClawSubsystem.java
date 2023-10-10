f47eeed40855edaba0c98a5620848513fbad61a8
Yinn333 <hoda.ata18@gmail.com>
2023-10-10 09:58:13 -0700

    finished set up

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystems/ClawSubsystem.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystems/ClawSubsystem.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.twenty403.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.technototes.library.hardware.servo.Servo;
 import com.technototes.library.logger.Loggable;
 import com.technototes.library.subsystem.Subsystem;
 @Config
@@ -8,4 +9,24 @@ public class ClawSubsystem implements Subsystem, Loggable {
     public static double OPEN_CLAW_POS = 0;
     public static double CLOSE_CLAW_POS = 1;
 
+
+    private Servo clawServo;
+    private Servo elbowServo;
+    private Servo shoulderServo;
+    private boolean haveHardware;
+
+
+    public ClawSubsystem (Servo claw, Servo elbow, Servo shoulder){
+    clawServo = claw;
+    elbowServo = elbow;
+    shoulderServo = shoulder;
+    haveHardware = true;
+    }
+    public ClawSubsystem (){
+    clawServo = null;
+    elbowServo = null;
+    shoulderServo = null;
+    haveHardware = false;
+    }
+    public void open(){}
 }
