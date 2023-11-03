eefa5fd54bd311a7462dbf277cc9bec205fed7b1
FTC16750 <technototes@outlook.com>
2023-11-02 18:40:30 -0700

    Vision constants for 20403 are good

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/Setup.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/Setup.java
@@ -9,9 +9,9 @@ public class Setup {
 
         public static boolean DRIVEBASE = true;
         public static boolean CLAWSUBSYSTEM = true;
-        public static boolean DRONESUBSYSTEM = true;
+        public static boolean DRONESUBSYSTEM = false;
         public static boolean WEBCAM = true;
-        public static boolean HANGSUBSYSTEM = true;
+        public static boolean HANGSUBSYSTEM = false;
     }
 
     @Config
