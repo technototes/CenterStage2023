be4b7f5c0a6afe970c11376767687bab069c8bbf
FTC16750 <technototes@outlook.com>
2023-11-04 15:00:32 -0700

    Validated 16750 placement subsystem(lift and arm).

---
+++ Sixteen750/src/main/java/org/firstinspires/ftc/sixteen750/commands/placement/ArmServoOutputCommand.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.sixteen750.commands.placement;
+
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.sixteen750.subsystems.PlacementSubsystem;
+
+public class ArmServoOutputCommand implements Command {
+    private PlacementSubsystem placement;
+
+    public ArmServoOutputCommand(PlacementSubsystem i) {
+        placement = i;
+        addRequirements(i);
+    }
+
+    @Override
+    public void execute() {
+        placement.ArmServoOutput();
+    }
+}
+
