e576f362ca842b5c865a24ef7494029d3de27767
Kevin Frei <kevinfrei@hotmail.com>
2023-11-04 11:49:53 -0700

    Drone settings

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystems/DroneSubsystem.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/subsystems/DroneSubsystem.java
@@ -1,14 +1,15 @@
 package org.firstinspires.ftc.twenty403.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.technototes.library.hardware.servo.Servo;
 import com.technototes.library.logger.Loggable;
 import com.technototes.library.subsystem.Subsystem;
-
+@Config
 public class DroneSubsystem implements Subsystem, Loggable {
 
     private Servo launchServo;
     private boolean haveHardware;
-    public static double launch_pos;
+    public static double launch_pos = 0.3;
 
     public DroneSubsystem(Servo l) {
         launchServo = l;
@@ -34,4 +35,5 @@ public class DroneSubsystem implements Subsystem, Loggable {
             launchServo.setPosition(c);
         }
     }
+
 }
