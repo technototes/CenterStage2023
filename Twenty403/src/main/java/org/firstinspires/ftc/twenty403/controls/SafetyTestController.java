ce8c75e980c58bae8cd3ddb8124c2c5b6a045314
FTC16750 <technototes@outlook.com>
2024-01-02 17:57:15 -0800

    Created commands for all 6 possible things that can break in order to simulate them failing started to add them to controller (safety controller), but nothing binded yet Added a timer in SafetySubsystem and make it check everything every half second Added SafetySubsystem toggle in setup - harshini 1/2/24

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/controls/SafetyTestController.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.twenty403.controls;
+
+import com.technototes.library.command.CommandScheduler;
+import com.technototes.library.control.CommandAxis;
+import com.technototes.library.control.CommandButton;
+import com.technototes.library.control.CommandGamepad;
+import com.technototes.library.control.Stick;
+
+import org.firstinspires.ftc.twenty403.Robot;
+import org.firstinspires.ftc.twenty403.Setup;
+import org.firstinspires.ftc.twenty403.commands.driving.JoystickDriveCommand;
+import org.firstinspires.ftc.twenty403.commands.driving.NormalModeCommand;
+import org.firstinspires.ftc.twenty403.commands.driving.ResetGyroCommand;
+import org.firstinspires.ftc.twenty403.commands.driving.SnailModeCommand;
+import org.firstinspires.ftc.twenty403.commands.driving.TurboModeCommand;
+
+public class SafetyTestController {
+
+    public Robot robot;
+    public CommandGamepad gamepad;
+
+    public Stick driveLeftStick, driveRightStick;
+    public CommandButton resetGyroButton;
+    public CommandButton turboButton;
+    public CommandButton snailButton;
+    public CommandAxis straightTrigger;
+
+    public SafetyTestController(CommandGamepad g, Robot r) {
+        this.robot = r;
+        gamepad = g;
+
+        AssignNamedControllerButton();
+        if (Setup.Connected.SAFETYSUBSYSTEM) {
+            bindDriveControls();
+        }
+    }
+
+    private void AssignNamedControllerButton() {
+        resetGyroButton = gamepad.ps_options;
+        driveLeftStick = gamepad.leftStick;
+        driveRightStick = gamepad.rightStick;
+        turboButton = gamepad.rightBumper;
+        snailButton = gamepad.leftBumper;
+        straightTrigger = gamepad.rightTrigger;
+    }
+
+    public void bindDriveControls() {
+        CommandScheduler
+            .getInstance()
+            .scheduleJoystick(
+                new JoystickDriveCommand(
+                    robot.drivebaseSubsystem,
+                    driveLeftStick,
+                    driveRightStick,
+                    straightTrigger
+                )
+            );
+
+        turboButton.whenPressed(new TurboModeCommand(robot.drivebaseSubsystem));
+        turboButton.whenReleased(new NormalModeCommand(robot.drivebaseSubsystem));
+
+        snailButton.whenPressed(new SnailModeCommand(robot.drivebaseSubsystem));
+        snailButton.whenReleased(new NormalModeCommand(robot.drivebaseSubsystem));
+
+        resetGyroButton.whenPressed(new ResetGyroCommand(robot.drivebaseSubsystem));
+    }
+}
