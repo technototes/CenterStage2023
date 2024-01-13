7ed17df771c1de0bdeae59e1c3e83a912425a861
Braelyn <siennafox@gmail.com>
2024-01-13 11:31:24 -0800

    large dec

---
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/arm/ShoulderLargeDecrementCommand.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.twenty403.commands.arm;
+
+import com.technototes.library.command.Command;
+
+import org.firstinspires.ftc.twenty403.subsystems.ArmSubsystem;
+
+public class ShoulderLargeDecrementCommand implements Command {
+
+    private ArmSubsystem subsystem;
+
+    public ShoulderLargeDecrementCommand(ArmSubsystem s) {
+        subsystem = s;
+        addRequirements(s);
+    }
+
+    @Override
+    public void execute() {
+        subsystem.shoulder_largeDecrement();
+    }
+}
