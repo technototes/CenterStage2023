087aec839202a229570555737d9304094497048e
Braelyn <siennafox@gmail.com>
2024-06-11 17:32:46 -0700

    fixed some values, changed some speeds, please merge

--- Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/arm/ArmSecondLineCommand.java
+++ Twenty403/src/main/java/org/firstinspires/ftc/twenty403/commands/arm/ArmSecondLineCommand.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.twenty403.commands.arm;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.technototes.library.command.Command;
 import org.firstinspires.ftc.twenty403.subsystems.ArmSubsystem;
-
+@Config
 public class ArmSecondLineCommand implements Command {
 
     private ArmSubsystem subsystem;
